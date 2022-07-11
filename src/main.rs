1ca1395f01dc05e26b6d209199138493607c00bc
Quiarom <userquiarom@gmail.com>
2022-07-10 22:13:44 -0400

    Edit tables

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,22 @@ fn main() {
 
     let connection_db = PgConnection::establish(&db_url).expect("Nou Possible Connection with Database");
 
-    use self::models::Post;
+    use self::models::{Post, NewPost};
+    use self::schema::posts;
     use self::schema::posts::dsl::*;
-    let posts_result = posts.load::<Post>(&connection_db).expect("Error ejecute query");
+
+    let new_posts = NewPost {
+        title: "Mi primer blog",
+        body: "Es mi primera oración",
+        slug: "primer-post",
+    };
+
+    
+    diesel::insert_into(posts::table).values(&new_posts).get_result::<Post>(&connection_db).expect("Error Insert Tables");
+    // let post: Post = diesel::insert_into(posts::table).values(&new_posts).get_result(&connection_db).expect("Error Insert Tables");
+
+    // Select * from posts limit 1
+    let posts_result = posts.limit(1).load::<Post>(&connection_db).expect("Error ejecute query");
 
     for post in posts_result {
        println!("{}", post.title); 
