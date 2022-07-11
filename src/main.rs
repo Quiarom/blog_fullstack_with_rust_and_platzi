b08b5ae120619f7079e05a80c51af1058dfa5f7d
Quiarom <userquiarom@gmail.com>
2022-07-10 21:35:33 -0400

    Create models (tables)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#[macro_use]
+extern crate diesel;
+
+pub mod schema;
+pub mod models;
+
+
+use dotenv::dotenv;
+use std::env;
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").expect("Not Found db_url Variable");
+
+    let connection_db = PgConnection::establish(&db_url).expect("Nou Possible Connection with Database");
+
+    use self::models::Post;
+    use self::schema::posts::dsl::*;
+    let posts_result = posts.load::<Post>(&connection_db).expect("Error ejecute query");
+
+    for post in posts_result {
+       println!("{}", post.title); 
+    }
 }
